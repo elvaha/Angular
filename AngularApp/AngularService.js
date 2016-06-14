c1941c9f41f4270c1d117aaf744597e6dc7a271d
elvaha <elias.v.hansen@gmail.com>
2016-06-15 01:18:49 +0200

    it works
    angular is king

--- AngularApp/AngularService.js
+++ AngularApp/AngularService.js
@@ -1,20 +1,22 @@
-(function(){
-   "use strict";
-    
+(function () {
+    "use strict";
+
     angular
         .module("shop")
         .factory("shopService", shopService);
-    
-    function shopService($http){
-        
-        function getData(){
-            return $http.get('/data/products.json').then(
-                function(response){
+
+    function shopService($http) {
+
+        var getData = function () {
+            return $http.get("/data/products.json")
+                .then(function (response) {
                     return response.data;
                 });
-        }
-        return {
-            getData : getData
         };
-    }     
-});
+        return {
+            getData: getData
+        };
+
+    }
+
+}());
--- AngularApp/app.js
+++ AngularApp/app.js
@@ -1,57 +1,54 @@
+(function () {
+    "use strict";
 
-(function(){
-	"use strict";
+    var app = angular.module("shop", [])
 
-	angular
-      .module("shop", [])
-      .controller("shopController", shopController);
-  
+    app.controller("shopController", shopController);
+
     function shopController($scope, shopService) {
-        
-        $scope.cart = {};   
-        
-      shopService.getData().then(function(data){
-        $scope.products = data;  
-      });
-      
-      $scope.viewProduct = function(product){
-          $scope.selectedProduct = product.title;
-          console.log(product);
-      }
-      
-      $scope.buyProduct = function(){
-          //console.log(this.product);
-          //console.log($scope.cart);
-          
-          var product = this.product; 
-          var cartObject = $scope.cart[product.id];
-          
-          if(!cartObject){
-              $scope.cart[product.id] = {
-                  "product" : this.product,
-                  "items" : 1
-              }
-              console.log("First time");
-          }
-          else {
-              cartObject.items++;
-          }
-      }
-          
-      $scope.deleteFromCart = function(){
-          
-          console.log(this.item);
-          var productId = this.cartItem.product.id;
-          
-          var cartObject = $scope.cart[productId];
-              
-          if(cartObject.items > 1){
-            cartObject.items--;
-          }
-          else{
-            delete $scope.cart[productId];
-          }
-      }
+
+        $scope.cart = {};
+
+        shopService.getData().then(function (data) {
+            $scope.products = data;
+        });
+
+        $scope.viewProduct = function (product) {
+            $scope.selectedProduct = product.title;
+            console.log(product);
+        }
+
+        $scope.buyProduct = function () {
+            //console.log(this.product);
+            //console.log($scope.cart);
+
+            var product = this.product;
+            var cartObject = $scope.cart[product.id];
+
+            if (!cartObject) {
+                $scope.cart[product.id] = {
+                    "product": this.product,
+                    "items": 1
+                }
+                console.log("First time");
+            } else {
+                cartObject.items++;
+            }
+        }
+
+        $scope.deleteFromCart = function () {
+
+            console.log(this.item);
+            var productId = this.cartItem.product.id;
+
+            var cartObject = $scope.cart[productId];
+
+            if (cartObject.items > 1) {
+                cartObject.items--;
+            } else {
+                delete $scope.cart[productId];
+            }
+        }
     }
 
 })();
